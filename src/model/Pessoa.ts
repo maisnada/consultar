d26296eea1d0541881027b4c8abe740a045a192d
Marcelo Palmito <maisnada@gmail.com>
2021-01-19 22:15:50 -0300

    add field n in method anoNascimento

--- src/model/Pessoa.ts
+++ src/model/Pessoa.ts
@@ -6,12 +6,14 @@ class Pessoa{
     constructor(nome:string, idade:number){
 
         this.nome = nome;
-        this.idade = idade;
-
-        const oi:string;
+        this.idade = idade;        
     }
 
     anoNascimento(): number{
+
+        let n:number = 5;
+
+        n = n + 2;
 
         return new Date().getFullYear() - this.idade;
 
